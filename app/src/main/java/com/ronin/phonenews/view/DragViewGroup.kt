f78150e723402f923b8632ef0db2bd94cdc33696
donghl <465430474@qq.com>
2017-07-31 18:08:31 +0800

    添加view的滑动测试代码

---
+++ app/src/main/java/com/ronin/phonenews/view/DragViewGroup.kt
@@ -0,0 +1,57 @@
+package com.ronin.phonenews.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.widget.LinearLayout
+
+/**
+ * Created by Administrator on 2017/7/31.
+ */
+class DragViewGroup : LinearLayout {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+    var lastx: Int? = null
+    var lasty: Int? = null
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val rawx = event?.rawX?.toInt()
+        val rawy = event?.rawY?.toInt()
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                lastx = rawx
+                lasty = rawy
+            }
+            MotionEvent.ACTION_MOVE -> {
+
+                val dx = rawx?.minus(lastx!!)
+                val dy = rawy?.minus(lasty!!)
+
+//                layout(left.plus(dx!!), top.plus(dy!!), right.plus(dx), bottom.plus(dy))
+
+//                offsetLeftAndRight(dx!!)
+//                offsetTopAndBottom(dy!!)
+
+                scrollBy(-dx!!,-dy!!)
+
+                lastx = rawx
+                lasty = rawy
+
+            }
+            MotionEvent.ACTION_UP -> {
+
+            }
+        }
+
+        return true
+    }
+
+
+}
+
