aea516da509f49fcb9bea492a95417583e681872
donghailong <donghailong@guazi.com>
2018-04-17 10:58:11 +0800

    add customactivityoncrash lib support

--- app/src/main/java/com/ronin/phonenews/application/App.kt
+++ app/src/main/java/com/ronin/phonenews/application/App.kt
@@ -5,6 +5,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.ronin.cc.util.G_SP_NAME
 import com.ronin.phonenews.util.ThreadPoolUtils
+import cat.ereza.customactivityoncrash.config.CaocConfig
+import com.ronin.cc.util.NeverHandler
+import com.ronin.cc.util.toast
+import com.ronin.phonenews.R
+import com.ronin.phonenews.activity.MainActivity
+import com.ronin.phonenews.activity.NewsActivity
+
 
 /**
  * Created by Administrator on 2017/7/27.
@@ -20,16 +27,26 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         _inst = this
-
+        initCrachConfig()
         gSP = getSharedPreferences(G_SP_NAME, Context.MODE_PRIVATE)
         ThreadPoolUtils.init()
 
-//        NeverHandler.init(object : NeverHandler.CrashHandler {
-//            override fun uncaughtException(service: Thread, e: Throwable) {
-//                toast(e.toString())
-//            }
-//        })
+    }
 
+    private fun initCrachConfig() {
+        CaocConfig.Builder.create()
+//                .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM
+//                .enabled(false) //default: true
+//                .showErrorDetails(false) //default: true
+//                .showRestartButton(false) //default: true
+//                .logErrorOnRestart(false) //default: true
+//                .trackActivities(true) //default: false
+//                .minTimeBetweenCrashesMs(2000) //default: 3000
+//                .errorDrawable(R.drawable.) //default: bug image
+//                .restartActivity(NewsActivity::class.java) //default: null (your app's launch activity)
+//                .errorActivity(YourCustomErrorActivity::class.java) //default: null (default error activity)
+//                .eventListener(YourCustomEventListener()) //default: null
+                .apply()
     }
 
 
